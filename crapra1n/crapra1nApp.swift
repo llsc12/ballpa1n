f684efc7ec0813e1a04341a15900bab601f18159
llsc12 <42747613+llsc12@users.noreply.github.com>
2022-10-13 22:06:27 +0100

    Initial Commit

---
+++ crapra1n/crapra1nApp.swift
@@ -0,0 +1,17 @@
+//
+//  crapra1nApp.swift
+//  crapra1n
+//
+//  Created by Lakhan Lothiyi on 13/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct crapra1nApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
