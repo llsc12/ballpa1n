6acbdb33c0b1343633a0e0273e1d078888adfd0d
llsc12 <42747613+llsc12@users.noreply.github.com>
2022-11-07 19:46:48 +0000

    fr

--- ballpa1n/ballpa1n.swift
+++ ballpa1n/ballpa1n.swift
@@ -20,7 +20,7 @@ struct ballpa1nApp: App {
                 .scaleEffect(triggerRespring ? 0.95 : 1)
                 .brightness(triggerRespring ? -1 : 0)
                 .statusBarHidden(triggerRespring)
-                .onChange(of: triggerRespring) { newValue in
+                .onChange(of: triggerRespring) { _ in
                     if triggerRespring == true {
                         Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false) { timer in
                             
---
+++ ballpa1n/pkgmgr/PackageManager.swift
@@ -0,0 +1,20 @@
+//
+//  PackageManager.swift
+//  ballpa1n
+//
+//  Created by Lakhan Lothiyi on 21/10/2022.
+//
+
+import SwiftUI
+
+struct PackageManager: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PackageManager_Previews: PreviewProvider {
+    static var previews: some View {
+        PackageManager()
+    }
+}
