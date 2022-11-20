414092ff6cf26b6a4e3026d73a4f98f92ff9bfdb
BomberFish <87151697+BomberFish@users.noreply.github.com>
2022-11-20 14:51:55 -0500

    Try and add bubble effect because cool
    xcode simulator is slow af

--- ballpa1n/jelbraik/ContentView.swift
+++ ballpa1n/jelbraik/ContentView.swift
@@ -14,19 +14,24 @@ struct ContentView: View {
     @State var currentStage: Int = 0
     @State var finished = false
     
+    @State var scale : CGFloat = 1
+    
     @ObservedObject var c = Console.shared
     
     var body: some View {
-        VStack {
-            title
-            
-            statusbar
-            
-            console
-            
-            controls
-            
-            disclaimer
+        ZStack {
+            bubbles
+            VStack {
+                title
+                
+                statusbar
+                
+                console
+                
+                controls
+                
+                disclaimer
+            }
         }
     }
     
@@ -46,6 +51,35 @@ struct ContentView: View {
         }
         .padding(.vertical, 4)
         .padding(.horizontal)
+    }
+    
+    @ViewBuilder
+    var bubbles: some View {
+        ZStack {
+                    ForEach (1...50, id:\.self) { _ in
+                        Circle ()
+                            .foregroundColor(Color (red: .random(in: 0.7...1),
+                                                    green: .random(in: 0.7...1),
+                                                    blue: .random(in: 0...0.1)))
+                        
+                            .blendMode(.colorDodge) // The bottom circle is lightened by an amount determined by the top layer
+                            .animation (Animation.spring (dampingFraction: 0.5)
+                                            .repeatForever()
+                                            .speed (.random(in: 0.05...0.4))
+                                            .delay(.random (in: 0...1)), value: scale
+                            )
+                        
+                            .scaleEffect(self.scale * .random(in: 0.1...3))
+                            .frame(width: .random(in: 1...100),
+                                   height: CGFloat.random (in:20...100),
+                                   alignment: .center)
+                            .position(CGPoint(x: .random(in: 0...1112),
+                                              y: .random (in:0...834)))
+                    }
+                }
+                .onAppear {
+                    self.scale = 1.2 // default circle scale
+                }
     }
     
     @ViewBuilder
@@ -366,8 +400,6 @@ launchctl,
     ]),
 ]
 
-#warning("Unknown on 28")
-
 struct StageStep {
     let status: String
     let avgInterval: Float
@@ -385,6 +417,7 @@ extension ConsoleStep: Equatable {
         return lhs.delay == rhs.delay && lhs.line == rhs.line
     }
 }
+
 
 struct PreviewIos: PreviewProvider {
     static var previews: some View {
